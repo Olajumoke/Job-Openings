8572b6bfc106fc3a4e31215f8aa788d7f7b75e76
Olajumoke <oguns.ca@hotmail.com>
2016-01-08 15:18:25 +0100

    Modified templates to display posts from database.

--- application/views.py
+++ application/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import JobOpenings
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'application/post_list.html',{})
+	posts = JobOpenings.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'application/post_list.html',{'posts': posts})
 	
