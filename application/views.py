c21ef17fa6bbe191d50320cecedd0d88d254c84c
Olajumoke <oguns.ca@hotmail.com>
2016-01-11 11:35:56 +0100

    Added view and Template for detailed Job post as well as CSS for the Site.

--- application/views.py
+++ application/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import JobOpenings
 from django.utils import timezone
+
 
 # Create your views here.
 def post_list(request):
 	posts = JobOpenings.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'application/post_list.html',{'posts': posts})
+	
+def post_detail(request, pk):
+	post = get_object_or_404(JobOpenings, pk=pk)
+	return render(request, 'application/post_detail.html', {'post': post})
 	
